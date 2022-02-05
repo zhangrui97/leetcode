4f09a8d15224a8ac0953dc33d320eaa3da95a07f
zhangrui97 <zhangrui97@outlook.com>
2022-02-05 00:22:33 -0800

    Time: 160 ms (97.01%), Space: 51.4 MB (12.01%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -5,6 +5,7 @@
 var isPalindrome = function(x) {
   if(x < 0 || (x !== 0 && x % 10 == 0))
         return false
-  const str = ''+x
-  return str === [...str].reverse().join('')
+  const isPal = s => 
+    s.length < 2 || (s[0] === s.at(-1) && isPal(s.slice(1, -1)))
+  return isPal(''+x)
 };
