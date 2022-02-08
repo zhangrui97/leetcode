cd320dcb2a516b54994aa33b838961ec9d37bedf
zhangrui97 <zhangrui97@outlook.com>
2022-02-07 16:02:01 -0800

    Time: 161 ms (25.10%), Space: 39.3 MB (46.94%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cs
+++ 2-add-two-numbers/2-add-two-numbers.cs
@@ -10,23 +10,34 @@
  * }
  */
 public class Solution {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        var result = new ListNode();
-        var p1 = l1;
-        var p2 = l2;
-        var carry = 0;
-        var next = result;
-        while (p1 != null || p2 != null) {
-            int sum = carry + (p1?.val ?? 0) + (p2?.val ?? 0);
-            next.next = new ListNode(sum % 10);
-            p1 = p1?.next;
-            p2 = p2?.next;
-            next = next.next;
-            carry = sum / 10;
-        }
-        if (carry > 0) {
-          next.next = new ListNode(carry);
-        }
-        return result.next;
+  static private ListNode AddCarry(int carry, ListNode l) {
+    if (carry == 0) return l;
+    else {
+      if (l == null) return new ListNode(carry);
+      if (l.val == 9) return new ListNode(0, AddCarry(1, l.next));
+      l.val += 1;
+      return l;
     }
+  }
+  public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+    var result = new ListNode();
+    var p1 = l1;
+    var p2 = l2;
+    var carry = 0;
+    var p = result;
+    while (p1 != null && p2 != null) {
+      int sum = carry + p1.val + p2.val;
+      p.next = new ListNode(sum % 10);
+      p1 = p1.next;
+      p2 = p2.next;
+      p = p.next;
+      carry = sum / 10;
+    }
+    if (p1 == null) {
+      p.next = AddCarry(carry, p2);
+    } else {
+      p.next = AddCarry(carry, p1);
+    }
+    return result.next;
+  }
 }
