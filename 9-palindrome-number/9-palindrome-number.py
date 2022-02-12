949c1e717a50a28291218e52cdafe1ca80b1dfd8
zhangrui97 <zhangrui97@outlook.com>
2022-02-12 09:06:18 -0800

    Time: 75 ms (61.55%), Space: 13.8 MB (91.22%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,4 +1,4 @@
 class Solution:
   def isPalindrome(self, x: int) -> bool:
-    isPal = lambda s : len(s) < 2 or (s[0] == s[-1] and isPal(s[1:-1]))
-    return isPal(str(x))
+    s = str(x)
+    return s == s[::-1]
