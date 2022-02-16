452ceb5d4c5a3173083c3f4f544a2fabfa70f808
zhangrui97 <zhangrui97@outlook.com>
2022-02-16 14:46:19 -0800

    Time: 164 ms (68.25%), Space: 47.4 MB (27.59%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -5,16 +5,12 @@
 var romanToInt = function(s) {
   const map = {I: 1, V: 5, X: 10, L: 50, C:100, D:500, M:1000}
   let sum = 0
-  let i = 0
-  for (; i < s.length - 1; i++) {
+  for (let i = 0; i < s.length; i++) {
     if (map[s[i]] < map[s[i+1]]) {
-      sum += map[s[i+1]] - map[s[i]]
-      i++
+      sum -= map[s[i]]
     } else {
       sum += map[s[i]]
     }
   }
-  if (i < s.length)
-    return sum + map[s[i]]
-  else return sum
+  return sum
 };
