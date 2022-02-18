5af254a195efc7da727b8873729e54db113d26fe
zhangrui97 <zhangrui97@outlook.com>
2022-02-18 10:56:39 -0800

    Time: 832 ms (63.57%), Space: 73.8 MB (33.68%) - LeetHub

--- 1-two-sum/1-two-sum.scala
+++ 1-two-sum/1-two-sum.scala
@@ -1,15 +1,15 @@
-import scala.collection.immutable.HashMap
+import scala.collection.mutable.HashMap
 
 object Solution {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    var map: HashMap[Int, Int] = HashMap[Int, Int]()
+    var map: HashMap[Int, Int] = HashMap.empty[Int, Int]
     for (i <- 0 until nums.length) {
       val n = nums(i)
       if (map.contains(n)) {
         return Array(map(n), i)
       }
-      map += (target - n) -> i
+      map.put(target - n, i)
     }
-    return Array.empty
+    return Array.empty[Int]
   }
 }
