c257b05e8d49d870c0bb9bd84f1bf4be163394ef
zhangrui97 <zhangrui97@outlook.com>
2022-02-23 11:26:00 -0800

    Time: 948 ms (27.45%), Space: 68.5 MB (45.10%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.scala
+++ 12-integer-to-roman/12-integer-to-roman.scala
@@ -15,12 +15,48 @@ object Solution {
       case 9 => s"$one$ten"
     }
   }
+  val d2r = collection.immutable.HashMap(
+    (0, 0) -> digitToRoman(0, 0),
+    (0, 1) -> digitToRoman(1, 0),
+    (0, 2) -> digitToRoman(2, 0),
+    (0, 3) -> digitToRoman(3, 0),
+    (0, 4) -> digitToRoman(4, 0),
+    (0, 5) -> digitToRoman(5, 0),
+    (0, 6) -> digitToRoman(6, 0),
+    (0, 7) -> digitToRoman(7, 0),
+    (0, 8) -> digitToRoman(8, 0),
+    (0, 9) -> digitToRoman(9, 0),
+    (1, 0) -> digitToRoman(0, 1),
+    (1, 1) -> digitToRoman(1, 1),
+    (1, 2) -> digitToRoman(2, 1),
+    (1, 3) -> digitToRoman(3, 1),
+    (1, 4) -> digitToRoman(4, 1),
+    (1, 5) -> digitToRoman(5, 1),
+    (1, 6) -> digitToRoman(6, 1),
+    (1, 7) -> digitToRoman(7, 1),
+    (1, 8) -> digitToRoman(8, 1),
+    (1, 9) -> digitToRoman(9, 1),
+    (2, 0) -> digitToRoman(0, 2),
+    (2, 1) -> digitToRoman(1, 2),
+    (2, 2) -> digitToRoman(2, 2),
+    (2, 3) -> digitToRoman(3, 2),
+    (2, 4) -> digitToRoman(4, 2),
+    (2, 5) -> digitToRoman(5, 2),
+    (2, 6) -> digitToRoman(6, 2),
+    (2, 7) -> digitToRoman(7, 2),
+    (2, 8) -> digitToRoman(8, 2),
+    (2, 9) -> digitToRoman(9, 2),
+    (3, 0) -> digitToRoman(0, 3),
+    (3, 1) -> digitToRoman(1, 3),
+    (3, 2) -> digitToRoman(2, 3),
+    (3, 3) -> digitToRoman(3, 3),
+  )
   def intToRoman(num: Int): String = {
     var result = ""
     var rest = num
     var pos = 0
     while (rest > 0) {
-      result = s"${digitToRoman(rest%10, pos)}$result"
+      result = s"${d2r(pos, rest%10)}$result"
       pos = pos + 1
       rest = rest / 10
     }
