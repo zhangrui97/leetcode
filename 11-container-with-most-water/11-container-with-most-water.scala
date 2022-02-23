742ff913c9fbbe80eba04db3a9d1862d54fd60c8
zhangrui97 <zhangrui97@outlook.com>
2022-02-23 10:52:03 -0800

    Time: 772 ms (84.03%), Space: 77.1 MB (78.99%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.scala
+++ 11-container-with-most-water/11-container-with-most-water.scala
@@ -1,28 +1,15 @@
 object Solution {
   def maxArea(height: Array[Int]): Int = {
-    var li = 0
-    var ri = height.length - 1
-    var l = height(li)
-    var r = height(ri)
-    var maxH = 0
+    var (li, ri) = (0, height.length - 1)
     var max = 0
     while (li < ri) {
-      val minH = math.min(l, r)
-      if (minH > maxH) {
-        maxH = minH
-        max = math.max(max, maxH * (ri - li))
-      }
-      if (l < r) {
+      max = max max ((height(li) min height(ri)) * (ri - li))
+      val (l, r) = (height(li), height(ri))
+      if (l <= r) {
         li += 1
-        l = height(li)
-      } else if (l > r) {
+      } 
+      if (l >= r) {
         ri -= 1
-        r = height(ri)
-      } else {
-        li += 1
-        l = height(li)
-        ri -= 1
-        r = height(ri)
       }
     }
     max
