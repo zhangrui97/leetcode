d5a39329e36d996b38ca76abe91a5fe46c9bb8a3
zhangrui97 <zhangrui97@outlook.com>
2022-03-02 22:35:06 -0800

    Time: 113 ms (20.22%), Space: 42.6 MB (12.59%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  const search = (l, r) => {
+    console.log(l, r)
+    if (nums[l] >= target) return l
+    if (nums[r] === target) return r
+    if (nums[r] < target) return r + 1
+    const mid = l + (~~((r-l) / 2))
+    if (nums[mid] == target) return mid
+    else if (nums[mid] < target) return search(mid + 1, r)
+    else return search(l, mid - 1)
+  }
+  return search(0, nums.length - 1)
+};
