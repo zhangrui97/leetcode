b56e347b2dfed2013b77eb994d3e0836d197fc30
zhangrui97 <zhangrui97@outlook.com>
2022-03-04 16:01:21 -0800

    Time: 56 ms (98.49%), Space: 42.4 MB (18.39%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -6,13 +6,12 @@
 var removeElement = function(nums, val) {
   const len = nums.length
   if (len === 0) return 0
-  if (len === 1) return nums[0] === val ? 0 : 1
   let fast = 0
   while (fast < len && nums[fast] != val) fast++
   let slow = fast
   while (fast < len) {
-    while (fast < len && nums[fast] == val) fast++
-    if (fast < len)
+    if (nums[fast] == val) fast++
+    else
       nums[slow++] = nums[fast++]
   }
   return slow  
