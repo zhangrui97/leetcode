5d9014011defc7a244b924813f348eb9dcd16b3e
zhangrui97 <zhangrui97@outlook.com>
2022-03-04 22:33:19 -0800

    Time: 75 ms (87.67%), Space: 44.8 MB (9.61%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,12 +4,10 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-  const a1 = nums.slice(0, n)
-  const a2 = nums.slice(n)
   const result = []
   for (let i = 0; i < n; i++) {
-    result.push(a1[i])
-    result.push(a2[i])
+    result.push(nums[i])
+    result.push(nums[n+i])
   }
   return result
 };
