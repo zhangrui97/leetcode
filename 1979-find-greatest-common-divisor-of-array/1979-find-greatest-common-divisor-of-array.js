3693b441b545c2d88bfd749b1456b6774d30329f
zhangrui97 <zhangrui97@outlook.com>
2022-03-06 12:08:52 -0800

    Time: 113 ms (25.66%), Space: 42.3 MB (48.67%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var findGCD = function(nums) {
-  const gcd = (a, b) => (a > b) ? gcd(b, a) : (a === 0) ? b : gcd(b % a, a)
+  const gcd = (a, b) => a ? gcd(b % a, a) : b
   return gcd(Math.min(...nums), Math.max(...nums))
 };
