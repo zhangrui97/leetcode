341bfc4221f5fda9796b17719da0a0224214b6fa
zhangrui97 <zhangrui97@outlook.com>
2022-03-05 20:14:08 -0800

    Time: 83 ms (74.68%), Space: 41.8 MB (72.78%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -5,9 +5,8 @@
 var sumOddLengthSubarrays = function(arr) {
   const l = arr.length
   const sum = arr.reduce((a, b) => a + b)
-  const subSub = len => {
-    if (len === 1) return sum
-    if (len === l) return sum
+  const sumOfSub = len => {
+    if (len === 1 || len === l) return sum
     const factor = l >= 2 * len ? len - 1: l - len
     let ans = (factor + 1) * sum
     for (let i = 0; i < factor; i++) {
@@ -17,7 +16,7 @@ var sumOddLengthSubarrays = function(arr) {
   }
   let result = 0
   for (let i = 1; i <= l; i += 2) {
-    result += subSub(i)
+    result += sumOfSub(i)
   }
   return result
 };
