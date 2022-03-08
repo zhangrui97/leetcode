505143dc2f94a01db7bac580f2cea05ff62c1ae1
zhangrui97 <zhangrui97@outlook.com>
2022-03-08 12:26:47 -0800

    Time: 77 ms (77.56%), Space: 42 MB (77.56%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.js
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.js
@@ -3,9 +3,14 @@
  * @return {boolean}
  */
 var canBeIncreasing = function(nums) {
-  return nums.some((_, i) => {
-    const arr = nums.slice()
-    arr.splice(i, 1)
-    return arr.every((n, i) => i === 0 || n > arr[i-1])
-  })
+  let once = 0
+  for (let i = 1; i < nums.length; i ++) {
+    if (nums[i] <= nums[i-1]) {
+      if (i === 1 || i === nums.length - 1) once += 1
+      else if (nums[i] > nums[i-2] || nums[i+1] > nums[i-1]) once += 1
+      else return false
+    }
+    if (once > 1) return false
+  }
+  return true
 };
