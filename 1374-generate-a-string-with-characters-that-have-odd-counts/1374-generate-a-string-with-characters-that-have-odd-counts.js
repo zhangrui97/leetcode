baf7fa8f5108474e17479308af0918f52e3f3e4b
zhangrui97 <zhangrui97@outlook.com>
2022-03-11 11:46:10 -0800

    Time: 62 ms (90.91%), Space: 42.3 MB (40.56%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -3,18 +3,5 @@
  * @return {string}
  */
 var generateTheString = function(n) {
-  const dict = "abcdefghi"
-  const generator = (m, i) => {
-    if (m === 1) return [dict[i]]
-    if ((m & 3) === 3) {
-      return new Array(m >> 1).fill(dict[i]).concat(generator((m >> 1) + 1, i+1))
-    } else if ((m & 1) === 1) {
-      return new Array((m >> 1) + 1).fill(dict[i]).concat(generator(m >> 1, i+1))
-    } else if ((m & 3) === 0) {
-      return new Array((m >> 1) + 1).fill(dict[i]).concat(new Array((m >> 1) - 1).fill(dict[i+1]))
-    } else { // if (m & 1 === 0)
-      return new Array(m >> 1).fill(dict[i]).concat(new Array(m >> 1).fill(dict[i+1]))
-    }
-  }
-  return generator(n, 0).join('')
+  return n%2 ? Array(n).fill('x').join('') : 'x' + Array(n-1).fill('y').join('')
 };
