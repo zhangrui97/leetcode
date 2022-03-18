732385b91a9917060321eeda9a24359558312af4
zhangrui97 <zhangrui97@outlook.com>
2022-03-17 21:52:31 -0700

    Time: 85 ms (49.44%), Space: 41.7 MB (80.63%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -3,7 +3,9 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-  const ans = new Array(rowIndex + 1).fill(1)
+  const ans = new Array(rowIndex + 1)
+  ans[0] = 1
+  ans[rowIndex] = 1
   let i = 1
   let f = rowIndex
   for (; i < ~~((rowIndex+1)/2); i++, f--) {
