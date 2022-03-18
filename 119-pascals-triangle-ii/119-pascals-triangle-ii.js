9e6fdc99478c5ec2823ee10d22367ecd34d32c73
zhangrui97 <zhangrui97@outlook.com>
2022-03-17 21:55:30 -0700

    Time: 70 ms (72.87%), Space: 42 MB (58.88%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -3,16 +3,24 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-  const ans = new Array(rowIndex + 1)
-  ans[0] = 1
-  ans[rowIndex] = 1
-  let i = 1
-  let f = rowIndex
-  for (; i < ~~((rowIndex+1)/2); i++, f--) {
-    const v = ans[i-1] * f / i
-    ans[i] = v
-    ans[rowIndex-i] = v
+  switch (rowIndex) {
+    case 0: return [1]
+    case 1: return [1, 1]
+    case 2: return [1, 2, 1]
+    case 3: return [1, 3, 3, 1]
+    case 4: return [1, 4, 6, 4, 1]
+    default:
+      const ans = new Array(rowIndex + 1)
+      ans[0] = 1
+      ans[rowIndex] = 1
+      let i = 1
+      let f = rowIndex
+      for (; i < ~~((rowIndex+1)/2); i++, f--) {
+        const v = ans[i-1] * f / i
+        ans[i] = v
+        ans[rowIndex-i] = v
+      }
+      if (rowIndex % 2 === 0) ans[i] = ans[i-1] * f / i
+      return ans
   }
-  if (rowIndex > 0 && rowIndex % 2 === 0) ans[i] = ans[i-1] * f / i
-  return ans
 };
