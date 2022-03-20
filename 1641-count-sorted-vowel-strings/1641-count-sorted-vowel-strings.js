4711c6c0faf6fc8b10a7878a4dbdd995fa05b5a7
zhangrui97 <zhangrui97@outlook.com>
2022-03-20 12:17:11 -0700

    Time: 91 ms (55.75%), Space: 41.5 MB (90.27%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var countVowelStrings = function(n) {
-  const oneLetter = 5
   const twoLetter = (n-1) * 10
-  const threeLetter = (n-1)*(n-2)/2 * 10
-  const fourLetter = (n-1)*(n-2)*(n-3)/6 * 5
-  const fiveLetter = (n-1)*(n-2)*(n-3)*(n-4)/24
-  return oneLetter + twoLetter + threeLetter + fourLetter + fiveLetter
+  const threeLetter = twoLetter*(n-2)/2
+  const fourLetter = threeLetter*(n-3)/6
+  const fiveLetter = fourLetter*(n-4)/20
+  return 5 + twoLetter + threeLetter + fourLetter + fiveLetter
 };
