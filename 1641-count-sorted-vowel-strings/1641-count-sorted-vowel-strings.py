cf2c3a46f184d1ce069529bdc72be021aa0bdc31
zhangrui97 <zhangrui97@outlook.com>
2022-03-20 12:27:11 -0700

    Time: 42 ms (63.43%), Space: 13.9 MB (73.48%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+  def countVowelStrings(self, n: int) -> int:
+    twoLetters = (n-1) * 10
+    threeLetters = twoLetters*(n-2)//2
+    fourLetters = threeLetters*(n-3)//6
+    fiveLetters = fourLetters*(n-4)//20
+    return 5 + twoLetters + threeLetters + fourLetters + fiveLetters
+        
