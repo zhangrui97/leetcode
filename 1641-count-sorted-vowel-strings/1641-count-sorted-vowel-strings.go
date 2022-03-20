b7d28406abc15e3cdae2a45d098d64b349884786
zhangrui97 <zhangrui97@outlook.com>
2022-03-20 13:16:16 -0700

    Time: 0 ms (100.00%), Space: 1.9 MB (46.94%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.go
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.go
@@ -1,7 +1,12 @@
 func countVowelStrings(n int) int {
-  twoLetters := (n-1) * 10
-  threeLetters := twoLetters*(n-2)/2
-  fourLetters := threeLetters*(n-3)/6
-  fiveLetters := fourLetters*(n-4)/20
-  return 5 + twoLetters + threeLetters + fourLetters + fiveLetters
+  /*
+    put all 5 vowels to the string of length (n + 5), remove all of them once in the string will be result
+    so the question becomes find the composition of four positions from the (n + 5 - 1) potential insert positions
+    The answer is C(n+4, 4), it is -
+    (n+1)*(n+2)*(n+3)*(n+4)/(1*2*3*4) 
+      = (n*n + 5*n + 4)(n*n + 5*n +6)/24
+      = (((n*n + 5*n)+10)*(n*n + 5*n) + 24)/24
+  */
+  x := n * (n + 5)
+  return (x * (x + 10) + 24) / 24
 }
