1adff4539d97ad5fa4f1780899b61d0728963d66
zhangrui97 <zhangrui97@outlook.com>
2022-03-23 23:32:44 -0700

    Time: 0 ms (100.00%), Space: 1.9 MB (81.22%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.go
+++ 392-is-subsequence/392-is-subsequence.go
@@ -1,24 +1,10 @@
 func isSubsequence(s string, t string) bool {
-  sl, tl := len(s), len(t)
-  if sl == 0 { return true }
-  if tl < sl { return false }
-  
-  i, j := 0, 0
-  for ; i < len(s); i++ {
-    ch := s[i]
-    for ; j < len(t); j++ {
-      if t[j] == ch {
-        j++
-        break
-      }
-    }
-    if j == len(t) {
-      if t[j-1] != ch {
-        return false
-      }
+  i := 0
+  for j := 0;i < len(s) && j < len(t); j++ {
+    if t[j] == s[i] {
       i++
-      break
     }
   }
+  
   return i == len(s)
 }
