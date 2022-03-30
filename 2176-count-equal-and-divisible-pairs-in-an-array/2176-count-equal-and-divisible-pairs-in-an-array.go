7acc32f8120350356d83ea6597074b1d26561503
zhangrui97 <zhangrui97@outlook.com>
2022-03-30 08:53:28 -0700

    Time: 3 ms (97.22%), Space: 3.3 MB (20.14%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.go
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.go
@@ -1,10 +1,16 @@
 func countPairs(nums []int, k int) int {
+  idxMap := make(map[int][]int)
+  for i, v := range nums {
+    idxMap[v] = append(idxMap[v], i)
+  }
   ans := 0
-  for i := 0; i < len(nums) - 1; i++ {
-    vi := nums[i]
-    for j:= i+1; j < len(nums); j++ {
-      if vi == nums[j] && (i*j)%k == 0 {
-        ans++
+  for _, v := range idxMap {
+    for i := 0; i < len(v) - 1; i++ {
+      idxi := v[i]
+      for j := i+1; j < len(v); j++ {
+        if idxi * v[j] % k == 0 {
+          ans++
+        }
       }
     }
   }
