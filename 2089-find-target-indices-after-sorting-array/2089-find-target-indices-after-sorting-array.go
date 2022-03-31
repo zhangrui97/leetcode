e71b200b7f910d7a25ffb1508c9e7e33f1a18d70
zhangrui97 <zhangrui97@outlook.com>
2022-03-31 10:29:45 -0700

    Time: 6 ms (44.54%), Space: 2.8 MB (78.99%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.go
@@ -0,0 +1,31 @@
+func targetIndices(nums []int, target int) []int {
+  sort.Ints(nums)
+  l, r := 0, len(nums) - 1
+  for l <= r {
+    mid := (l + r) / 2
+    if nums[mid] < target {
+      l = mid + 1
+    } else {
+      r = mid - 1
+    }
+  }
+  left := l
+  l, r = 0, len(nums) - 1
+  for l <= r {
+    mid := (l+r) / 2
+    if nums[mid] > target {
+      r = mid - 1
+    } else {
+      l = mid + 1
+    }
+  }
+  if left > r {
+    return []int{}
+  } else {
+    var ans []int
+    for i := left; i <= r; i++ {
+      ans = append(ans, i)
+    }
+    return ans
+  }
+}
