4d2252c3bcfd72c16563d514ddef25f1b3da2491
zhangrui97 <zhangrui97@outlook.com>
2022-04-01 17:46:01 -0700

    Time: 71 ms (75.64%), Space: 42.1 MB (99.74%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -3,7 +3,14 @@
  * @return {number}
  */
 var findNumbers = function(nums) {
+  const isEvenDigits = num => {
+    if (num > 999) {
+      return num <= 9999 || num == 100000
+    } else {
+      return num > 9 && num <= 99
+    }
+  }
   let ans = 0
-  nums.forEach(n => ans += (('' + n).length % 2 === 0))
+  nums.forEach(n => ans += isEvenDigits(n))
   return ans
 };
