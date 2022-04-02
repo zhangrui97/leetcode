74296c2050729b4189f316e10e8d55a2c59b6726
zhangrui97 <zhangrui97@outlook.com>
2022-04-02 12:12:45 -0700

    Time: 11 ms (97.14%), Space: 6.5 MB (58.57%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.go
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.go
@@ -1,15 +1,22 @@
+func findFirstNegativeIndex(row []int, l int) int {
+  r := len(row) - 1
+  for l <= r {
+    mid := (l + r) / 2
+    if row[mid] >= 0 {
+      l = mid + 1
+    } else {
+      r = mid - 1
+    }
+  }
+  return l
+}
 func countNegatives(grid [][]int) int {
   m := len(grid)
   n := len(grid[0])
-  var j int
+  j := 0
   ans := 0
   for i := m - 1; i >= 0; i -- {
-    row := grid[i]
-    for ; j < n; j ++ {
-      if row[j] < 0 {
-        break
-      }
-    }
+    j = findFirstNegativeIndex(grid[i], j)
     ans += n - j
   }
   return ans
