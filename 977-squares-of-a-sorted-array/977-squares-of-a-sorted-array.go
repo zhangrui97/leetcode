bef8f72d63be445fbce42948799fe3e314d577c8
zhangrui97 <zhangrui97@outlook.com>
2022-04-03 00:16:11 -0700

    Time: 39 ms (54.85%), Space: 8.5 MB (8.22%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.go
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.go
@@ -1,7 +1,26 @@
 func sortedSquares(nums []int) []int {
-  for i := 0; i < len(nums); i ++ {
-    nums[i] = nums[i] * nums[i]
+  n := len(nums)
+  l, r := 0, n - 1
+  nums[0] = nums[0] * nums[0]
+  if n == 1 {
+    return nums
+  }
+  nums[r] = nums[r] * nums[r]
+  ans := make([]int, n)
+  for i := n - 1; i >= 0 ; i -- {
+    if nums[l] > nums[r] {
+      ans[i] = nums[l]
+      l ++
+      if l != r && l < n {
+        nums[l] = nums[l]*nums[l]
+      }
+    } else {
+      ans[i] = nums[r]
+      r --
+      if l != r && r >= 0 {
+        nums[r] = nums[r]*nums[r]
+      }
+    }
   }  
-  sort.Ints(nums)
-  return nums
+  return ans
 }
