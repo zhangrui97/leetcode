1f2299a82f8f30ab078304c373d0c9185576a342
zhangrui97 <zhangrui97@outlook.com>
2022-04-03 10:22:23 -0700

    Time: 4 ms (98.11%), Space: 3.7 MB (49.06%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.go
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.go
@@ -1,13 +1,7 @@
-func bits(num int) int {
-  if num == 0 {
-    return 0
-  }
-  return 1 + bits(num & (num-1))
-}
+import "math/bits"
 
 func sortByBits(arr []int) []int {
   sort.Slice(arr, func(i, j int) bool {
-    return bits(arr[i]) << 16 | arr[i] < bits(arr[j]) << 16 | arr[j]
-  })    
+    return bits.OnesCount(uint(arr[i])) << 16 | arr[i] < bits.OnesCount(uint(arr[j])) << 16 | arr[j]})    
   return arr
 }
