3c422ae679a0f197e5856faa0ff89946d829ffbf
zhangrui97 <zhangrui97@outlook.com>
2022-04-02 23:18:02 -0700

    Time: 8 ms (42.11%), Space: 3.2 MB (89.47%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.go
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.go
@@ -1,16 +1,19 @@
 func minSubsequence(nums []int) []int {
+  n := len(nums)
   sort.Ints(nums)
   sum := 0
   for _, v := range nums {
     sum += v
   }
-  ans := make([]int, 0, len(nums))
   max := 0
-  for i := len(nums) - 1; i >= 0; i-- {
-    ans = append(ans, nums[i])
+  for i := n - 1; i >= 0; i-- {
     max += nums[i]
     if (max<<1) > sum {
-      return ans
+      
+      for j := i; j < (n+i)/2; j ++ {
+        nums[j], nums[n-1-j+i] = nums[n-1-j+i], nums[j]
+      }
+      return nums[i:]
     }
   }
   return []int{}
