824aa9372ded7526150811d0c737275baf1fa6db
zhangrui97 <zhangrui97@outlook.com>
2022-04-04 23:57:53 -0700

    Time: 144 ms (22.29%), Space: 47.2 MB (41.43%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -7,12 +7,13 @@ var combinationSum = function(candidates, target) {
   const ans = []
   const backtrack = (cand, stack, sum) => {
     for (let i = 0; i < cand.length; i++) {
-      const s = [...stack, cand[i]]
-      const next = sum + cand[i]
+      const v = cand[i]
+      const next = sum + v
       if (next < target) {
-        backtrack(cand.slice(i), s, next)
+        backtrack(cand.slice(i), [...stack, v], next)
       } else if (next === target) {
-        ans.push(s)
+        stack.push(v)
+        ans.push(stack)
       } else return
     }
   }
