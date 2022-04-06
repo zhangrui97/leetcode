43a876f5596c960b6c7f7eff80c189e86ca925ea
zhangrui97 <zhangrui97@outlook.com>
2022-04-06 11:38:16 -0700

    Time: 75 ms (62.34%), Space: 42.5 MB (78.85%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,10 +4,39 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-  const fst = nums.indexOf(target)
-  if (fst === -1) {
-    return [-1, -1]
-  } else {
-    return [fst, nums.lastIndexOf(target)]
+  let l = 0
+  let r = nums.length - 1
+  let mid = 0
+  while (l <= r) {
+    mid = ~~((l+r)/2)
+    if (nums[mid] == target) break
+    if (nums[mid] > target) {
+      r = mid - 1
+    } else {
+      l = mid + 1
+    }
   }
+  if (l > r) return [-1, -1]
+  let ll = l
+  let lr = mid
+  let m = l
+  while (ll < lr) {
+    m = ~~((ll+lr)/2)
+    if (nums[m] < target) {
+      ll = m + 1
+    } else {
+      lr = m
+    }
+  }
+  let rl = mid
+  let rr = r + 1
+  while (rl < rr) {
+    m = ~~((rl+rr)/2)
+    if (nums[m] > target) {
+      rr = m
+    } else {
+      rl = m + 1
+    }
+  }
+  return [ll, rl-1]  
 };
