f40e444934a80d3cab5181a1dbbe499ec5983659
zhangrui97 <zhangrui97@outlook.com>
2022-04-06 12:16:22 -0700

    Time: 88 ms (91.35%), Space: 15.4 MB (54.67%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,21 @@
 class Solution:
   def searchRange(self, nums: List[int], target: int) -> List[int]:
-    def search() -> Tuple[int, int, int]:
-      l, r = 0, len(nums)-1
-      while l <= r:
-        mid = (l+r)//2
-        if nums[mid] == target: return l, mid, r
-        if nums[mid] > target: 
-          r = mid - 1
-        else:
-          l = mid + 1
-      return 0, -1, 0
+    def search(l, r) -> Tuple[int, int, int]:
+      if l > r: return 0, -1, 0
+      mid = (l+r)//2
+      if nums[mid] == target: return l, mid, r
+      if nums[mid] > target: return search(l, mid-1)
+      else: return search(mid + 1, r)
     def searchLeft(l, r) -> int:
-      while l < r:
-        mid = (l+r)//2
-        if nums[mid] < target:
-          l = mid + 1
-        else:
-          r = mid
-      return l
+      if l == r: return l
+      mid = (l+r)//2
+      if nums[mid] < target: return searchLeft(mid + 1, r)
+      else:return searchLeft(l, mid)
     def searchRight(l, r) -> int:
-      while l < r:
-        mid = (l+r)//2
-        if nums[mid] > target:
-          r = mid
-        else:
-          l = mid + 1
-      return l - 1
-    l, m, r = search()
+      if l == r: return l-1
+      mid = (l+r)//2
+      if nums[mid] > target: return searchRight(l, mid)
+      else: return searchRight(mid + 1, r)
+    l, m, r = search(0, len(nums)-1)
     if m == -1: return [-1, -1]
     else: return [searchLeft(l, m), searchRight(m, r+1)]
