16d9d90eb6c257f07adf601f55aa1b94d64bd102
zhangrui97 <zhangrui97@outlook.com>
2022-04-06 19:20:14 -0700

    Time: 788 ms (74.71%), Space: 16.8 MB (25.29%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.go
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.go
@@ -4,37 +4,21 @@ type NumMatrix struct {
 
 
 func Constructor(matrix [][]int) NumMatrix {
+  m, n := len(matrix), len(matrix[0])
+  sums := make([][]int, m+1)
+  sums[0] = make([]int, n+1)
   for i, r := range matrix {
+    row := make([]int, n+1)
+    sums[i+1] = row
     for j, v := range r {
-      if i == 0 {
-        if j == 0 {
-          matrix[0][0] = v
-        } else {
-          matrix[i][j] = v + matrix[i][j-1]
-        }
-      } else if j == 0 {
-        matrix[i][0] = v + matrix[i-1][0]
-      } else {
-        matrix[i][j] = v + matrix[i-1][j] + matrix[i][j-1] - matrix[i-1][j-1]
-      }
+      row[j+1] = v + sums[i][j+1] + row[j] - sums[i][j]
     }
   }
-  return NumMatrix{matrix}  
+  return NumMatrix{sums}  
 }
 
-
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
-  result := this.sums[row2][col2]
-  if row1 > 0 {
-    result -= this.sums[row1-1][col2]
-  } 
-  if col1 > 0 {
-    result -= this.sums[row2][col1-1]
-  }
-  if row1 > 0 && col1 > 0 {
-    result += this.sums[row1-1][col1-1]
-  }
-  return result
+  return this.sums[row2+1][col2+1] - this.sums[row2+1][col1] - this.sums[row1][col2+1] + this.sums[row1][col1]
 }
 
 
