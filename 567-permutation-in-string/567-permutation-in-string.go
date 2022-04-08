f92b01361d851bfb5b61199d965733f20ba8db1a
zhangrui97 <zhangrui97@outlook.com>
2022-04-08 14:41:16 -0700

    Time: 16 ms (41.54%), Space: 3.8 MB (24.02%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.go
+++ 567-permutation-in-string/567-permutation-in-string.go
@@ -1,26 +1,28 @@
 func checkInclusion(s1 string, s2 string) bool {
+  l1, l2 := len(s1), len(s2)
+  if l2 < l1 { return false }
   target := make(map[byte]int, len(s1))
   for _, v := range s1 { target[byte(v)]++ }
   targetl := len(target)
   window := make(map[byte]int, len(s1))
   valid := 0
   l, r := 0, 0
-  for r < len(s2) {
+  for r < l2 {
     rc := s2[r]
     r++
     if val, ok := target[rc]; ok {
       window[rc]++
       if val == window[rc] {
         valid ++
+        if (valid == targetl) {
+          return true
+        }
       }
     }
-    for valid == targetl {
+    if r >= l1 {
       lc := s2[l]
       if val, ok := target[lc]; ok {
         if val == window[lc] {
-          if r - l == len(s1) {
-            return true
-          }
           valid--
         }
         window[lc]--
