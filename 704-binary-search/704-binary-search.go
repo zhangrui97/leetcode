173002148fdce4025b78822d555c4fe8369f1893
zhangrui97 <zhangrui97@outlook.com>
2022-04-10 18:50:57 -0700

    Time: 36 ms (69.35%), Space: 7.1 MB (42.07%) - LeetHub

---
+++ 704-binary-search/704-binary-search.go
@@ -0,0 +1,10 @@
+func search(nums []int, target int) int {
+  l, r := 0, len(nums)-1
+  for l <= r {
+    m := l + (r-l)/2
+    if nums[m] == target { return m }
+    if nums[m] < target { l = m+1 }
+    if nums[m] > target { r = m-1 }
+  }
+  return -1
+}
