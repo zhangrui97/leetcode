63c55a695f5c0dcd7f96422bc291cab2975db761
zhangrui97 <zhangrui97@outlook.com>
2022-04-10 18:20:48 -0700

    Time: 14 ms (36.97%), Space: 7.7 MB (13.79%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.c
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.c
@@ -1,17 +1,9 @@
-int search(int* nums, int target, int* l, int* r){
-  if (*l > *r) return -1;
-  int m = (*l + *r) / 2;
-  if (nums[m] == target) return m;
-  if (nums[m] > target) *r = m - 1;
-  else *l = m + 1;
-  return search(nums, target, l, r);
-}
-int searchLeft(int* nums, int target, int l, int r){
-  if (l == r) return l;
+int searchLeft(int* nums, int numsSize, int target, int l, int r){
+  if (l == r) return l >= numsSize ? -1 : nums[l] == target ? l : -1;
   int m = (l + r) / 2;
   if (nums[m] < target) l = m + 1;
   else r = m;
-  return searchLeft(nums, target, l, r);
+  return searchLeft(nums, numsSize, target, l, r);
 }
 int searchRight(int* nums, int target, int l, int r){
   if (l == r) return r-1;
@@ -27,14 +19,13 @@ int searchRight(int* nums, int target, int l, int r){
 int* searchRange(int* nums, int numsSize, int target, int* returnSize){
   int* result = (int*)malloc(2 * sizeof(int));
   *returnSize = 2;
-  int l = 0, r = numsSize - 1;
-  int mid = search(nums, target, &l, &r);
-  if (mid == -1) {
-    result[0] = -1;
-    result[1] = -1;
-  } else {
-    result[0] = searchLeft(nums, target, l, mid);
-    result[1] = searchRight(nums, target, mid, r + 1);
+  result[0] = -1;
+  result[1] = -1;
+  if (numsSize == 0) return result;
+  int left = searchLeft(nums, numsSize, target, 0, numsSize);
+  if (left != -1) {
+    result[0] = left;
+    result[1] = searchRight(nums, target, left+1, numsSize);
   }
   return result;
 }
