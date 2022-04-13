d9f3ccc7d48f47c13a917975d051f3b26f9083f6
zhangrui97 <zhangrui97@outlook.com>
2022-04-13 14:42:41 -0700

    Time: 36 ms (75.60%), Space: 7.8 MB (42.42%) - LeetHub

--- 15-3sum/15-3sum.go
+++ 15-3sum/15-3sum.go
@@ -1,32 +1,31 @@
 func threeSum(nums []int) [][]int {
-  sort.Ints(nums)
   length := len(nums)
   if length < 3 { return [][]int{} }
+  sort.Ints(nums)
   ans := make([][]int, 0, length - 2)
   ni := nums[0] - 1
   for i := 0; i < length - 2; i++ {
     for i < length - 2 && nums[i] == ni { i++ }
     ni = nums[i]
     if ni > 0 { break }
-    nj := nums[i+1] - 1
-    for j := i + 1; j < length - 1; j++ {
-      for j < length - 1 && nums[j] == nj { j++ }
-      nj = nums[j]
-      target := 0 - ni - nj
-      if nj > target { break }
-      l, r := j+1, length - 1
-      for l <= r {
-        m := (l+r) / 2
-        nk := nums[m]
-        if nk == target {
-          
-          ans = append(ans, []int{ni, nj, nk})
-          break
-        } else if nk < target {
-          l = m + 1
-        } else {
-          r = m - 1
-        }
+    l, r := i+1, length - 1
+    for l < r {
+      nj := nums[l]
+      if (ni + (nj<<1)) > 0 { break }
+      nk := nums[r]
+      sum := ni + nj + nk
+      if sum == 0 { 
+        ans = append(ans, []int{ni, nj, nk})
+        l ++
+        for ; l < r && nums[l] == nj; l++ {}
+        r --
+        for ; l < r && nums[r] == nk; r-- {}
+      } else if sum > 0 {
+        r --
+        for ; l < r && nums[r] == nk; r-- {}
+      } else {
+        l ++
+        for ; l < r && nums[l] == nj; l++ {}
       }
     }
   }
