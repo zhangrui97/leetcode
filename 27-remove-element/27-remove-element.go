b0d0cf7cc6812d3bbf5888cad5bd038a072feb1a
zhangrui97 <zhangrui97@outlook.com>
2022-04-14 17:29:17 -0700

    Time: 2 ms (36.91%), Space: 2 MB (79.19%) - LeetHub

--- 27-remove-element/27-remove-element.go
+++ 27-remove-element/27-remove-element.go
@@ -1,18 +1,10 @@
 func removeElement(nums []int, val int) int {
-  l, r := 0, len(nums) - 1
-  if r < 0 { return 0 }
-  for l < r {
-    for ; l < r && nums[l] != val; l++ {}
-    for ; l < r && nums[r] == val; r-- {}
-    if l < r {
-      nums[l], nums[r] = nums[r], nums[l]
-      l++
-      r--
+  s := 0
+  for _, v := range nums {
+    if v != val {
+      nums[s] = v
+      s++
     }
   }
-  if nums[l] == val { 
-    return l
-  } else { 
-    return l + 1
-  }
+  return s
 }
