7705b1e27ec1f07fbadbe464bff72c2e5835c0b1
zhangrui97 <zhangrui97@outlook.com>
2022-04-16 23:38:36 -0700

    Time: 12 ms (54.40%), Space: 4.5 MB (94.63%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -1,25 +1,22 @@
 func trap(height []int) int {
   length := len(height)
-  if length < 2 { return 0 }
   sum := 0
-  l, r := 0, length - 1
-  lmax := height[l]
-  rmax := height[r]
-  for l <= r {
+  l, lmax, rmax, r := 0, 0, 0, length - 1
+  for l < r {
     lv, rv := height[l], height[r]
-    if lv >= lmax {
-      lmax = lv
+    if lv < rv {
+      if lv > lmax {
+        lmax = lv
+      } else {
+        sum += lmax - lv
+      }
       l++
-    } else if lmax <= rmax {
-      sum += lmax - height[l]
-      l ++
-    }
-    if l > r { break }
-    if rv >= rmax {
-      rmax = rv
-      r--
-    } else if rmax <= lmax {
-      sum += rmax - height[r]
+    } else {
+      if rv >= rmax {
+        rmax = rv
+      } else {
+        sum += rmax - rv
+      }
       r --
     }
   }
