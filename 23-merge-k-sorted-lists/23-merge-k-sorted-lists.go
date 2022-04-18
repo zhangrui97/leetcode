ba82a0b870e82e8256d12f499c9aa9e712747cb6
zhangrui97 <zhangrui97@outlook.com>
2022-04-17 21:48:49 -0700

    Time: 115 ms (31.17%), Space: 6.8 MB (11.95%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.go
@@ -1,12 +1,23 @@
-func removeEmpty(lists []*ListNode) []*ListNode {
-  slow := 0
-  for _, v := range lists {
-    if v != nil {
-      lists[slow] = v
-      slow++
+func merge(l1, l2 *ListNode) *ListNode {
+  ans := &ListNode{}
+  p := ans
+  for l1 != nil && l2 != nil {
+    if l1.Val < l2.Val {
+      p.Next = l1
+      l1 = l1.Next
+    } else {
+      p.Next = l2
+      l2 = l2.Next
     }
+    p = p.Next
   }
-  return lists[:slow]
+  if l1 != nil {
+    p.Next = l1
+  }
+  if l2 != nil {
+    p.Next = l2
+  }
+  return ans.Next
 }
 
 /**
@@ -17,30 +28,12 @@ func removeEmpty(lists []*ListNode) []*ListNode {
  * }
  */
 func mergeKLists(lists []*ListNode) *ListNode {
-  lists = removeEmpty(lists)
-  switch len(lists) {
+  l := len(lists)
+  switch l {
   case 0: return nil
-    case 1: return lists[0]
+  case 1: return lists[0]
   default:
-    ans := &ListNode{}
-    p := ans
-    for len(lists) > 1 {
-      sm := 0
-      for i := 1; i < len(lists); i++ {
-        if lists[i].Val < lists[sm].Val {
-          sm = i
-        }
-      }
-      p.Next = lists[sm]
-      p = p.Next
-      if lists[sm].Next == nil {
-        lists[len(lists)-1], lists[sm] = lists[sm], lists[len(lists)-1]
-        lists = lists[:len(lists)-1]
-      } else {
-        lists[sm] = lists[sm].Next
-      }
-    }
-    p.Next = lists[0]
-    return ans.Next
+    lists[l-2] = merge(lists[l-2], lists[l-1])
+    return mergeKLists(lists[:l-1])
   }
 }
