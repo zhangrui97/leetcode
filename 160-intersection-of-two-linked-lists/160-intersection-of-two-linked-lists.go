feeb071af55e29aeb66c36de8a0720c1f3a7acbc
zhangrui97 <zhangrui97@outlook.com>
2022-04-19 11:42:14 -0700

    Time: 69 ms (13.15%), Space: 7.5 MB (37.59%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.go
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.go
@@ -7,21 +7,17 @@
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
   pa, pb := headA, headB
-  for pa != nil && pb != nil {
-    pa = pa.Next
-    pb = pb.Next
+  for pa != pb {
+    if pa == nil {
+      pa = headB
+    } else {
+      pa = pa.Next
+    }
+    if pb == nil {
+      pb = headA
+    } else {
+      pb = pb.Next
+    }
   }
-  p, pl, ps := pa, headA, headB
-  if pb != nil {
-    p, pl, ps = pb, headB, headA
-  }
-  for p != nil {
-    p = p.Next
-    pl = pl.Next
-  }
-  for pl != ps {
-    pl = pl.Next
-    ps = ps.Next
-  }
-  return pl
+  return pa
 }
