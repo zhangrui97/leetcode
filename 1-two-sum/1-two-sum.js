ee270be9213f44b7770f9f5d5ba88fc3032a70de
zhangrui97 <zhangrui97@outlook.com>
2022-04-25 20:55:18 -0700

    Time: 103 ms (57.80%), Space: 43.3 MB (26.95%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for (i = 0; i < nums.length; i++) {
-    for (j = 0; j < i; j++) {
-      if (nums[j] + nums[i] === target) {
-        return [i, j]
-      }
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i]
     }
+    map.set(target - nums[i], i)
   }
 };
