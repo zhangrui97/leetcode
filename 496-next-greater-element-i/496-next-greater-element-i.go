d08b2b06f9ec2ab72f9339fa844d95388803d863
zhangrui97 <zhangrui97@outlook.com>
2022-05-02 16:32:49 -0700

    Time: 4 ms (62.41%), Space: 3.5 MB (46.10%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.go
+++ 496-next-greater-element-i/496-next-greater-element-i.go
@@ -9,11 +9,11 @@ func nextGreaterElement(nums1 []int, nums2 []int) []int {
   }
   stack := make([]int, 0, len(nums2))
   for _, v := range nums2 {
-    for len(stack) > 0 && v > stack[len(stack)-1] {
-      if ind, ok := dict[stack[len(stack)-1]]; ok {
+    for l:=len(stack); l > 0 && v > stack[l-1]; l=len(stack) {
+      if ind, ok := dict[stack[l-1]]; ok {
         ans[ind] = v
       }
-      stack = stack[:len(stack)-1]
+      stack = stack[:l-1]
     }
     stack = append(stack, v)
   }
