de8e8be594e2094995124fb3dd413ba460d56769
zhangrui97 <zhangrui97@outlook.com>
2022-05-14 14:06:34 -0700

    Time: 125 ms (30.39%), Space: 51.7 MB (59.86%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -16,6 +16,5 @@ var lowestCommonAncestor = function(root, p, q) {
   const l = lowestCommonAncestor(root.left, p, q)
   const r = lowestCommonAncestor(root.right, p, q)
   if (l && r) return root
-  if (!l && !r) return null
-  return l ? l : r
+  return l || r
 };
