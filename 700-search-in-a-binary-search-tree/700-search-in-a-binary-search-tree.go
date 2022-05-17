4c5238d515551e9e4ef26346059c5de70f5eb105
zhangrui97 <zhangrui97@outlook.com>
2022-05-17 15:23:14 -0700

    Time: 25 ms (44.76%), Space: 7 MB (95.99%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.go
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.go
@@ -7,8 +7,10 @@
  * }
  */
 func searchBST(root *TreeNode, val int) *TreeNode {
-  if root == nil { return nil }
-  if val < root.Val { return searchBST(root.Left, val) }
-  if val > root.Val { return searchBST(root.Right, val) }
-  return root
+  if root == nil || root.Val == val { return root }
+  if val < root.Val { 
+    return searchBST(root.Left, val) 
+  } else { 
+    return searchBST(root.Right, val) 
+  }
 }
