2fe77fa60790cb6a75ef203381d44a9b973ced87
zhangrui97 <zhangrui97@outlook.com>
2022-05-18 16:13:31 -0700

    Time: 11 ms (7.30%), Space: 5.9 MB (5.84%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.go
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.go
@@ -10,13 +10,16 @@ func generateTrees(n int) []*TreeNode {
   size := []int{1, 1, 2, 5, 14, 42, 132, 429, 1430}
   var genTrees func(s, e int)[]*TreeNode 
   genTrees = func(s, e int)[]*TreeNode {
-    ans := make([]*TreeNode, 0, size[n])
     if s >= e { 
-      return append(ans, nil)
+      return []*TreeNode{nil}
     }
     if s+1 == e { 
-      return append(ans, &TreeNode{s, nil, nil})
+      return []*TreeNode{&TreeNode{s, nil, nil}}
     }
+    if s+2 == e {
+      return []*TreeNode{&TreeNode{s, nil, &TreeNode{s+1, nil, nil}}, &TreeNode{s+1, &TreeNode{s, nil, nil}, nil}}
+    }
+    ans := make([]*TreeNode, 0, size[n])
     for i := s; i < e; i++ {
       for _, vl := range genTrees(s, i) {
         for _, vr := range genTrees(i+1, e) {
