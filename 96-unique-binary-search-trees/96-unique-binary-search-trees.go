a1eb1a8cd4d2cd4a1dae8fc5c474068a7b5f756f
zhangrui97 <zhangrui97@outlook.com>
2022-05-18 10:02:17 -0700

    Time: 3 ms (14.74%), Space: 2 MB (37.37%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.go
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.go
@@ -1,14 +1,17 @@
 func numTrees(n int) int {
-  switch n {
-    case 0: return 1
-    case 1: return 1
-    case 2: return 2
-    case 3: return 5
-    default:
-      ans := 0
-      for i := 0; i < n; i++ {
-        ans += numTrees(i) * numTrees(n-1-i)
-      }
-      return ans
+  cach := make(map[int]int, n+1)
+  cach[0] = 1
+  var calc func(m int)int
+  calc = func(m int)int {
+    if v, ok := cach[m]; ok {
+      return v
+    }
+    ans := 0
+    for i := 0; i < m; i++ {
+      ans += calc(i) * calc(m-1-i)
+    }
+    cach[m] = ans
+    return ans
   }
+  return calc(n)
 }
