935f58d385229c747e15f9e51466ce4e9adfda17
zhangrui97 <zhangrui97@outlook.com>
2022-05-21 22:16:09 -0700

    Time: 70 ms (96.89%), Space: 8.6 MB (83.42%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.go
+++ 938-range-sum-of-bst/938-range-sum-of-bst.go
@@ -8,8 +8,9 @@
  */
 func rangeSumBST(root *TreeNode, low int, high int) int {
   if root == nil { return 0 }  
-  if root.Val < low { return rangeSumBST(root.Right, low, high) }
-  if root.Val > high { return rangeSumBST(root.Left, low, high) }
-  val := root.Val
-  return val + rangeSumBST(root.Left, low, val-1) + rangeSumBST(root.Right, val+1, high)
+  val := rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+  if root.Val >= low && root.Val <= high {
+    val += root.Val
+  }
+  return val
 }
