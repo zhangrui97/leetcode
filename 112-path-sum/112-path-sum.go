077a0cc3d1606e64b416cdb16fc98f6b7cc08a59
zhangrui97 <zhangrui97@outlook.com>
2022-05-22 22:56:23 -0700

    Time: 12 ms (15.16%), Space: 4.7 MB (68.95%) - LeetHub

--- 112-path-sum/112-path-sum.go
+++ 112-path-sum/112-path-sum.go
@@ -6,16 +6,11 @@
  *     Right *TreeNode
  * }
  */
-func dfs(t *TreeNode, sum, target int) bool {
-  if t == nil { return false }
-  sum += t.Val
-  if t.Left == nil && t.Right == nil {
-    if sum == target {
-      return true
-    }
-  }  
-  return dfs(t.Left, sum, target) || dfs(t.Right, sum, target)
-}
 func hasPathSum(root *TreeNode, targetSum int) bool {
-  return dfs(root, 0, targetSum)  
+  if root == nil { return false }
+  if root.Left == nil && root.Right == nil {
+    return root.Val == targetSum
+  }  
+  targetSum -= root.Val
+  return hasPathSum(root.Left, targetSum) || hasPathSum(root.Right, targetSum)
 }
