35a1451459784e84439410273c4a956503b507ef
zhangrui97 <zhangrui97@outlook.com>
2022-06-13 10:29:33 -0700

    Time: 62 ms (42.94%), Space: 13.9 MB (73.13%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,21 +1,21 @@
-class Solution(object):
-    def nextPermutation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums);
-        k = n - 2
-        while k >= 0 and nums[k] >= nums[k+1]:
-          k = k - 1
-        if k < 0:
-          nums[:] = nums[::-1]
-        else:
-          v = nums[k]
-          l = n - 1
-          while l > k and nums[l] <= v:
-            l = l - 1
-          nums[l], nums[k] = v, nums[l]
-          nums[k+1:] = nums[k+1:][::-1]
-
+class Solution:
+  def nextPermutation(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    k = n-2
+    while k >= 0 and nums[k] >= nums[k+1]:
+      k = k - 1
+    if k < 0:
+      nums[:] = nums[::-1]
+    else:
+      l = n-1
+      v = nums[k]
+      while l > k+1 and nums[l] <= v:
+        l = l-1
+      nums[k] = nums[l]
+      nums[l] = v
+      nums[k+1:] = nums[k+1:][::-1]
+      
         
