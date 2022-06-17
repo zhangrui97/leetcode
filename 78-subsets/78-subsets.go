19ef86704d3cb4881a9687180f89a3a70b4403fd
zhangrui97 <zhangrui97@outlook.com>
2022-06-17 09:47:35 -0700

    Time: 2 ms (50.16%), Space: 2.4 MB (23.36%) - LeetHub

--- 78-subsets/78-subsets.go
+++ 78-subsets/78-subsets.go
@@ -1,19 +1,17 @@
 func subsets(nums []int) [][]int {
   n := len(nums)
   ans := [][]int{}
-  var backtrack func(path []int, i int)
-  backtrack = func(path []int, i int) {
-    if i > n { return }
-    l := len(path)
-    res := make([]int, l)
-    copy(res, path)
+  var backtrack func(path int, i int)
+  backtrack = func(path int, i int) {
+    res := []int{}
+    for j, p := 0, path; p > 0; j, p = j+1, p>>1 {
+      if (p & 1) > 0 { res = append(res, nums[j]) }
+    }
     ans = append(ans, res)
     for j := i; j < n; j++ {
-      path = append(path, nums[j])
-      backtrack(path, j+1)
-      path = path[:l]
+      backtrack(path | (1 << j), j+1)
     }
   }
-  backtrack([]int{}, 0)
+  backtrack(0, 0)
   return ans
 }
