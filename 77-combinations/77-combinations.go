1ba8163dfec1032144f4e758834ceaaf9d19ffdc
zhangrui97 <zhangrui97@outlook.com>
2022-06-18 10:46:51 -0700

    Time: 21 ms (46.25%), Space: 6.2 MB (96.00%) - LeetHub

--- 77-combinations/77-combinations.go
+++ 77-combinations/77-combinations.go
@@ -1,19 +1,22 @@
 func combine(n int, k int) [][]int {
   ans := [][]int{}
-  var backtrack func(path []int, i, l int)
-  backtrack = func(path []int, i, l int) {
+  var backtrack func(path int, i, l int)
+  backtrack = func(path int, i, l int) {
     if l == k {
       res := make([]int, k)
-      copy(res, path)
+      for j, k, p := 0, 0, path; p > 0; j, p = j+1, p >> 1 {
+        if p & 1 > 0 { 
+          res[k] = j+1
+          k += 1
+        }
+      }
       ans = append(ans, res)
       return
     }
-    for j := i+1; j <= n; j++ {
-      path = append(path, j)
-      backtrack(path, j, l+1)
-      path = path[:l]
+    for j := i; j < n; j++ {
+      backtrack(path | (1 << j), j+1, l+1)
     }
   }
-  backtrack([]int{}, 0, 0)
+  backtrack(0, 0, 0)
   return ans
 }
