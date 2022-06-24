eeac8037256be15cdc76cefbc069a4feb8ecebe9
zhangrui97 <zhangrui97@outlook.com>
2022-06-23 23:12:39 -0700

    Time: 7 ms (70.02%), Space: 2.2 MB (92.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.go
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.go
@@ -8,18 +8,16 @@ func max(a, b int)int {
 
 func longestCommonSubsequence(text1 string, text2 string) int {
   n1, n2 := len(text1), len(text2)
-  result := 0
   dpPre, dpCur := make([]int, n2+1), make([]int, n2+1) 
   for i := 1; i <= n1; i++ {
     dpPre, dpCur = dpCur, dpPre
     for j := 1; j <= n2; j++ {
       if text1[i-1] == text2[j-1] {
         dpCur[j] = dpPre[j-1] + 1
-        result = max(dpCur[j], result)
       } else {
         dpCur[j] = max(dpPre[j], dpCur[j-1])
       }
     }
   }
-  return result
+  return dpCur[n2]
 }
