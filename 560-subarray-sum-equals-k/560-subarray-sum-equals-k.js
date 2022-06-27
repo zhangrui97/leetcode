67291e079ce23f0e2ba7b094446f939c1a80b761
zhangrui97 <zhangrui97@outlook.com>
2022-06-26 21:34:10 -0700

    Time: 149 ms (41.67%), Space: 53.3 MB (46.32%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
@@ -1,19 +1,23 @@
-let cnt = 0
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-
-  let counter = 0
-  const map = new Map()
-  map.set(0, 1)
+  const cache = {}
+  cache[0] = 1
   let sum = 0
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i]
-    counter += (map.get(sum-k) || 0)
-    map.set(sum, (map.get(sum) || 0)+1)
+  let count = 0
+  for (const n of nums) {
+    sum += n
+    if (cache[sum-k]) {
+      count += cache[sum-k]
+    }
+    if (cache[sum]) {
+      cache[sum]++
+    } else {
+      cache[sum]  = 1
+    }
   }
-  return counter
+  return count
 }
