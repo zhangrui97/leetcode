3895fe4d60558ba5ffe1085b9cf5853d1009465b
zhangrui97 <zhangrui97@outlook.com>
2022-06-30 10:26:58 -0700

    Time: 10 ms (93.17%), Space: 3.1 MB (74.53%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
@@ -1,33 +1,28 @@
-func max(a, b int)int {
-  if a > b {
-    return a
-  } else {
-    return b
-  }
-}
-
 func canPartition(nums []int) bool {
-  n := len(nums)
-  sum := 0
-  mx := 0
-  for _, v := range nums {
-    sum += v
-    if v > mx { mx = v }
-  }  
-  if sum % 2 == 1 || mx + mx > sum { return false }
-  sum /= 2
-  dp := make([][]int, n+1)
-  dp[0] = make([]int, sum+1)
-  for i := 1; i <= n; i++ {
-    v := nums[i-1]
-    dp[i] = make([]int, sum+1)
-    for j := 1; j <= sum; j++ {
-      if v > j {
-        dp[i][j] = dp[i-1][j]
-      } else {
-        dp[i][j] = max(dp[i-1][j], dp[i-1][j-v] + v)
-      }
-    }
-  }
-  return dp[n][sum] == sum
+  sum, max := 0, 0
+	for _, num := range nums {
+		sum += num
+    if num > max { max = num }
+	}
+	if sum%2 != 0 || max+max > sum {
+		return false
+	}
+	sum = sum / 2
+  fmt.Println(sum)
+	for range nums {
+    ans := 0
+    fmt.Println(nums)
+		for _, num := range nums {
+			if num+ans <= sum {
+				ans += num
+			}
+			if ans == sum {
+				return true
+			}
+		}
+		nums = append(nums, nums[0])
+		nums = nums[1:]
+	}
+
+	return false
 }
