63f3ffae67266a4e7e8d639e1548fcd331508b66
zhangrui97 <zhangrui97@outlook.com>
2022-06-30 20:30:52 -0700

    Time: 15 ms (87.00%), Space: 2.5 MB (91.95%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.go
@@ -1,13 +1,4 @@
-func max(a, b int)int {
-  if a > b {
-    return a
-  } else {
-    return b
-  }
-}
-
 func canPartition(nums []int) bool {
-  n := len(nums)
   sum := 0
   mx := 0
   for _, v := range nums {
@@ -17,20 +8,14 @@ func canPartition(nums []int) bool {
   if sum % 2 == 1 || mx + mx > sum { return false }
   sum /= 2
   if mx == sum { return true }
-  dpPre, dpCur := make([]int, sum+1), make([]int, sum+1)
-  for i := 1; i <= n; i++ {
-    dpCur, dpPre = dpPre, dpCur
-    v := nums[i-1]
-    for j := 1; j <= sum; j++ {
-      if j < v {
-        dpCur[j] = dpPre[j]
-      } else if j == v {
-        dpCur[j] = v
-      } else {
-        dpCur[j] = max(dpPre[j], dpPre[j-v] + v)
-        if dpCur[j] == sum { return true }
+  dp := make([]bool, sum+1)
+  dp[0] = true
+  for _, v := range nums {
+    for j := sum; j >= v; j-- {
+      if dp[j-v] {
+        dp[j] = true
       }
     }
   }
-  return dpCur[sum] == sum
+  return dp[sum]
 }
