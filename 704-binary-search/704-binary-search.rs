be43c7e5b09f3280ccdba90faa8dcebab65335c5
zhangrui97 <zhangrui97@outlook.com>
2022-07-03 11:55:33 -0700

    Time: 4 ms (75.51%), Space: 2.3 MB (38.66%) - LeetHub

--- 704-binary-search/704-binary-search.rs
+++ 704-binary-search/704-binary-search.rs
@@ -1,20 +1,19 @@
 impl Solution {
   pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-    let mut lo: usize = 0;
-    let mut hi: usize = nums.len() - 1;
-    while lo <= hi {
+    let mut ans = -1;
+    let (mut lo, mut hi) = (0, nums.len());
+    while lo < hi {
       let mid = (lo + hi) / 2;
-      if nums[mid] == target { return mid as i32 }
+      if nums[mid] == target { 
+        ans = mid as i32;
+        break;
+      }
       if nums[mid] > target {
-        if mid == 0 { 
-          return -1 
-        } else {
-          hi = mid - 1;
-        }
+        hi = mid;
       } else {
         lo = mid + 1;
       }
     }
-    -1
+    ans
   }
 }
