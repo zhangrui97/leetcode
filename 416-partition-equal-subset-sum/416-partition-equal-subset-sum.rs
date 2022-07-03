1f6442dc81f73c2be2da15072290810744bb922b
zhangrui97 <zhangrui97@outlook.com>
2022-07-03 16:31:42 -0700

    Time: 26 ms (81.08%), Space: 2.1 MB (97.30%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.rs
@@ -0,0 +1,15 @@
+impl Solution {
+  pub fn can_partition(nums: Vec<i32>) -> bool {
+    let (max, sum) = (nums.iter().max().unwrap(), nums.iter().sum());
+    if sum % 2 != 0 || max+max > sum { return false }
+    let target = (sum / 2) as usize;
+    let mut dp = vec![false; target + 1];
+    dp[0usize] = true;
+    for n in nums.iter() {
+      for i in (*n as usize..=target).rev() {
+        dp[i] = dp[i] || dp[i-(*n as usize)]
+      }
+    }
+    dp[target]
+  }
+}
