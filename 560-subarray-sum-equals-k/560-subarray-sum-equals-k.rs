1fd499d6d545256e16c30866e4a5e81d08fcc4bf
zhangrui97 <zhangrui97@outlook.com>
2022-07-04 10:40:27 -0700

    Time: 25 ms (51.35%), Space: 2.4 MB (83.78%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.rs
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.rs
@@ -2,17 +2,17 @@ use std::collections::HashMap;
 
 impl Solution {
   pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
-    let mut dict = HashMap::new();
+    let mut dict = HashMap::with_capacity(nums.len()+1);
     dict.insert(0, 1);
     let mut sum = 0;
     let mut counter = 0;
-    for &v in nums.iter() {
+    for v in nums {
       sum += v;
-      let key = sum-k;
-      if dict.contains_key(&key) {
-        counter += dict[&key];
+      if let Some(c) = dict.get(&(sum - k)) {
+        counter += c;
       }
-      dict.insert(sum, if dict.contains_key(&sum) { dict[&sum]+1 } else { 1 });
+      let e = dict.entry(sum).or_insert(0);
+      *e += 1;
     }
     counter
   }
