ae3fc831a61eee8d798c0e521da92eb20a51729b
zhangrui97 <zhangrui97@outlook.com>
2022-07-05 10:13:30 -0700

    Time: 2 ms (33.33%), Space: 2.3 MB (33.33%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.rs
+++ 1094-car-pooling/1094-car-pooling.rs
@@ -1,15 +1,13 @@
 impl Solution {
   pub fn car_pooling(trips: Vec<Vec<i32>>, capacity: i32) -> bool {
-    let mut diff = [0; 1000];
+    let mut diff = [0; 1001];
     let mut max = 0usize;
     for trip in trips {
       let (num, from, to) = (trip[0], trip[1] as usize, trip[2] as usize);
       if num > capacity { return false }
       if to > max { max = to }      
       diff[from] += num;
-      if to < 1000 {
-        diff[to] -= num;
-      }
+      diff[to] -= num;
     }
     let mut sum = 0;
     for i in 0usize..max {
