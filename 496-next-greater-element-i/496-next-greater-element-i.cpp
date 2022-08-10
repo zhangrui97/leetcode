8877ff64d351fc2a56823c7e1751664d04085c4d
zhangrui97 <zhangrui97@outlook.com>
2022-08-10 10:16:13 -0700

    Time: 12 ms (45.36%), Space: 9 MB (32.98%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
   vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    map<int, int> dict;
+    unordered_map<int, int> dict;
     stack<int> stack;
     for (int i = nums2.size()-1; i >= 0; i--) {
       auto v{nums2[i]};
       while (!stack.empty() && stack.top() <= v) {
         stack.pop();
       }
-      if (stack.empty()) {
-        dict.insert({v, -1});
-      } else {
-        dict.insert({v, stack.top()});        
-      }
+      dict.insert({v, stack.empty() ? -1 : stack.top()});
       stack.push(v);
     }
-    vector<int> ans(nums1.size(), -1);
+    vector<int> ans(nums1.size());
     for (int i = 0; i < nums1.size(); i++) {
-      auto v{nums1[i]};
-      if (dict.count(v)) {
-        ans[i] = dict[v];
-      }
+      ans[i] = dict[nums1[i]];
     }
     return ans;
   }
